ef9e6a10100071b17b3017d8d503c575f1796b5d
Kamil <kbury.dev@gmail.comgit config --global user.email kbury.dev@gmail.comgit config --global user.email kbury.dev@gmail.com>
2023-03-08 12:17:17 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { TypeAnimation } from 'react-type-animation';
 import './App.css';
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { !isClicked &&
+          <button className='button-50' onClick={() =>setIsClicked(true)}>Click me !</button>
+        }
+        { isClicked &&
+        <>
+         <TypeAnimation
+         sequence={[
+           `Hey Iza, Happy Women's Day! Entire Wizdo team wish You:`, // Types 'One'
+           1000, // Waits 1s
+           () => {
+             console.log('Done typing!'); // Place optional callbacks anywhere in the array
+           }
+         ]}
+         wrapper="div"
+         cursor={false}
+         repeat={0}
+         style={{ fontSize: '1.5em' }}
+       />
+       <TypeAnimation
+         sequence={[
+          ``, // Types 'One'
+           2700, // Waits 1s
+           1000, // Waits 1s
+           'Eslint throws no warnings.', // Deletes 'One' and types 'Two'
+           2000, // Waits 2s
+           'That recoil remembers that we sometimes are tired.',
+           2000, // Waits 2s
+           'Good documentation on that framework your implenting.',
+           2000, // Waits 2s
+           `Clients that don't make changes during development.`,
+           2000, // Waits 2s
+           `Lot of fun with coding.`,
+           2000, // Waits 2s
+           `Pizza day everyday!`,
+           2000, // Waits 2s
+           () => {
+             console.log('Done typing!'); // Place optional callbacks anywhere in the array
+           }
+         ]}
+         wrapper="div"
+         cursor={false}
+         repeat={Infinity}
+         style={{ fontSize: '1.5em' }}
+       />
+       </>
+        }
       </header>
     </div>
   );
